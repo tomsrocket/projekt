84ee92ba9da31d80efa79030d5869ddb5f528634
Thomas Werner <thomas.w.werner@gmail.com>
2025-02-04 22:12:15 +0100

    Add more interactive features

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -13,24 +13,42 @@ $(document).ready(function(){
   });
   //  return data.length
 
-  const mydate = new Date();
+  var mydate = new Date();
   const options = { weekday: "long" };
   const weekday = new Intl.DateTimeFormat("en-US", options).format(mydate);
   $(".vv-weekday").html(weekday);
   console.log("weekday", weekday)
 
-  const lessons = data["timetable-1"];
-  const lessonlist = lessons.split(",");
-  var currentLession = 0; 
-  for (const lesson of lessonlist) {
-    console.log("lesson", lesson);
-    $('#lessontable').append('<tr class="mint"><td>' + (++currentLession) + '</td><td>' + lesson + '</td></tr>')
-    if (currentLession==6) {
-        $('#lessontable').append('<tr class="table-secondary"><td>' + (++currentLession) + '</td><td>Lunch Break</td></tr>')
-    }
+  function getDayName(jdate) {
+    return jdate.toLocaleDateString("en-US", { weekday: 'long' });        
+  }
+  function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
   }
 
-  const todolist = data["todolist"];
+  function paintLessons(jdate) {
+    var dayNr = jdate.getDay();
+    console.log("draw lesson table for day", dayNr)
+    const lessons = data["timetable-" + dayNr];
+    $('#lessonsday').html(getDayName(jdate))
+    $('#lessontable').html('');
+    if (lessons) {
+      const lessonlist = lessons.split(",");
+      var currentLession = 0; 
+      for (const lesson of lessonlist) {
+        console.log("lesson", lesson);
+        $('#lessontable').append('<tr class="mint"><td>' + (++currentLession) + '</td><td>' + lesson + '</td></tr>')
+        if (currentLession==6) {
+            $('#lessontable').append('<tr class="table-secondary"><td>' + (++currentLession) + '</td><td>Lunch Break</td></tr>')
+        }
+      }
+    }
+  }
+  paintLessons(mydate);
+
+    const todolist = data["todolist"];
   const todos = todolist.split("\n");
   var currentTodo = 0
   for (const todo of todos) {
@@ -41,7 +59,6 @@ $(document).ready(function(){
         )
       }
    }
-
 
   var cloneCount = 0;
   $("#todolist input").click(function(){
@@ -59,5 +76,48 @@ $(document).ready(function(){
     toastBootstrap.show()
 
   }); 
+
+
+  $("#btnPrevDay").click(function(){
+    mydate = addDays(mydate, -1);
+    paintLessons(mydate);
+  });
+  $("#btnNextDay").click(function(){
+    mydate = addDays(mydate, 1);
+    paintLessons(mydate);
+  });
+
+
+  function convertH2M(timeInHour){
+    var timeParts = timeInHour.split(":");
+    return Number(timeParts[0]) * 60 + Number(timeParts[1]);
+  }
+  
+  var timeInMinutes = convertH2M("14:30");
+  console.log(timeInMinutes);
+
+  var startTime = convertH2M(data['startTime']);
+  var endTime = convertH2M(data['endTime']);
+
+  function timeFunc() {
+    const timeStr =  new Date().toLocaleTimeString('en-US', { hour12: false, 
+      hour: "numeric", 
+      minute: "numeric"});
+    const timeInt = convertH2M(timeStr);
+    if (timeInt > endTime) {
+      $('#progressBar').removeClass(['progress-bar-striped','progress-bar-animated'])
+    } 
+    const total = endTime-startTime;
+    const current = timeInt-startTime;
+    const progressInt = current/total*100;
+    const progressPercent = "" + parseInt(progressInt) + "%";
+    console.log("time current", timeStr, timeInt, current +"/"+total, progressPercent);  
+
+    $('#progressBar').html(timeStr);
+    $('#progressBar').css("width", progressPercent)
+  }
+  setTimeout(timeFunc, 20000);
+  timeFunc();
+
 });
 
